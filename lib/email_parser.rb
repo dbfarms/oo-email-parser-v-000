a24d83aed62f4b5eb21144a6d713464e30ebba36
Matthew Hunger <matt.hunger@gmail.com>
2017-12-03 15:40:36 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,9 +17,9 @@ class EmailParser
   def parse
 
     string_array = @string.split(/[,]| | /).select {|t| t.length > 3}
-    string_array
+    string_array = string_array.uniq 
 
-    binding.pry
+    #binding.pry
 
   end
 
